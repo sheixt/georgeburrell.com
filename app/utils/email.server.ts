693b99f658b0004884dedec38e45b8066ab26306
George Burrell <hello@georgeburrell.com>
2023-07-23 13:12:17 +0100

    hotfix: correct mail domain

--- app/utils/email.server.ts
+++ app/utils/email.server.ts
@@ -31,7 +31,7 @@ export async function sendEmail({
 	| { html: string; text: string; react?: never }
 	| { react: ReactElement; html?: never; text?: never }
 )) {
-	const from = 'system@georgeburrell.com'
+	const from = 'system@mail.georgeburrell.com'
 
 	const email = {
 		from,
