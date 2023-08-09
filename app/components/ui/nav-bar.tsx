daba3d6fbf1410884e01da9cc3069f4b0f507f14
George Burrell <hello@georgeburrell.com>
2023-08-09 12:31:46 +0100

    fix: logo display

--- app/components/ui/nav-bar.tsx
+++ app/components/ui/nav-bar.tsx
@@ -13,18 +13,26 @@ export function NavBar() {
 	return (
 		<header className="container py-6">
 			<nav className="flex items-center justify-between gap-4">
-				<ThemeSwitch userPreference={userPrefs.theme} />
+				<div className="w-48">
+					<ThemeSwitch userPreference={userPrefs.theme} />
+				</div>
 				<Link
 					to="/"
-					className="flex items-center gap-2 font-bold uppercase tracking-[1em] dark:text-white"
+					className="group flex items-center gap-4 font-bold uppercase tracking-[0.8em] dark:text-white"
 				>
-					<span className="hidden text-right opacity-20 lg:inline-flex">
-						George
+					<span className="relative left-3 hidden w-40 justify-end lg:inline-flex">
+						<span className="animate-bg-position bg-300% opacity-20 transition-all group-hover:bg-gradient-to-r group-hover:from-pink-800 group-hover:from-10% group-hover:via-purple-700 group-hover:to-teal-600 group-hover:bg-clip-text group-hover:text-transparent group-hover:opacity-100">
+							George
+						</span>
 					</span>
-					<Logo className="h-14 w-14" />
-					<span className="hidden pl-3 opacity-20 lg:inline-flex">Burrell</span>
+					<Logo className="h-14 w-14 group-hover:animate-spin group-hover:repeat-[0.5]" />
+					<span className="hidden w-40 lg:inline-flex">
+						<span className="animate-bg-position bg-300% opacity-20 transition-all group-hover:bg-gradient-to-r group-hover:from-pink-800 group-hover:from-10% group-hover:via-purple-700 group-hover:to-teal-600 group-hover:bg-clip-text group-hover:text-transparent group-hover:opacity-100">
+							Burrell
+						</span>
+					</span>
 				</Link>
-				<div className="flex items-center gap-10">
+				<div className="flex w-48 items-center justify-end gap-10">
 					{user ? (
 						<UserDropdown />
 					) : (
